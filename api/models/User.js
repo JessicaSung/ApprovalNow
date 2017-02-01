857f8a19b40822bf15f91dbb492646ccfa60654b
Weihao Rick Lee <kenchi133@hotmail.com>
2017-01-31 20:53:42 -0600

    api route

---
+++ api/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+var UserSchema = mongoose.Schema({
+	name: {
+		type: String,
+		index:true
+	},
+	employee_id:{
+		type:String,
+	},
+	email: {
+		type: String,
+	},
+	password: {
+		type: String,
+	}
+
+});
+
+
+// module.exports.createUser= function(newUser, callback){
+// 	var bcrypt = require('bcryptjs');
+// 	bcrypt.genSalt(10, function(err, salt) {
+//     bcrypt.hash(newUser.password, salt, function(err, hash) {
+//          newUser.password = hash;
+//          newUsser.save(callback);
+//     });
+// });
+
+// }
+var User = module.exports = mongoose.model('User', UserSchema);
