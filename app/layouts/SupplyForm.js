fecdc2c1a79f4c43b1bd2e5f76427288ae1657b0
JessicaSung <jessica.sung@utexas.edu>
2017-02-04 17:03:40 -0600

    Populated inventory items into table from an array.

--- app/layouts/SupplyForm.js
+++ app/layouts/SupplyForm.js
@@ -5,7 +5,7 @@ export default class SupplyForm extends Component {
 	render() {
 		return (
 			<div className="pageBlock">
-				{/*<Inventory />*/}
+				<Inventory />
 				<button className="floatRight btn btn-primary btn-lg">Add Items</button>
 			</div>
 		);
