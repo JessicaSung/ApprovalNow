c4d51335f32bc813d5aa821220c5e90193e91a42
JessicaSung <jessica.sung@utexas.edu>
2017-02-07 11:43:51 -0600

    Wireframed Purchase Requisition Form.

--- app/config/routes.js
+++ app/config/routes.js
@@ -6,6 +6,7 @@ import LoginSignOut from 'LoginSignOut';
 import LoginSignUp from 'LoginSignUp';
 import LoginSignUpSuccessful from 'LoginSignUpSuccessful';
 import Main from 'Main';
+import PurchaseRequisition from 'PurchaseRequisition';
 import SupplyForm from 'SupplyForm';
 import Welcome from 'Welcome';
 
@@ -13,10 +14,11 @@ module.exports = (
 	<Router history={hashHistory}>
 		<Route path="/" component={Main}>
 			<Route path="supplyform" component={SupplyForm} />
-			<Route path="signup" component={LoginSignUp}/>
-			<Route path="signupsuccessful" component={LoginSignUpSuccessful}/>
-			<Route path="signin" component={LoginSignIn}/>
-			<Route path="logout" component={LoginSignOut}/>
+			<Route path="signup" component={LoginSignUp} />
+			<Route path="signupsuccessful" component={LoginSignUpSuccessful} />
+			<Route path="signin" component={LoginSignIn} />
+			<Route path="logout" component={LoginSignOut} />
+			<Route path="purchaserequisition" component={PurchaseRequisition} />
 			<IndexRoute component={Welcome} />
 		</Route>
 	</Router>
